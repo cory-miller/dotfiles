e1c9a74e0c1d85acbb1066382a5fbec38e824b60
Cory Miller <13227161+cory-miller@users.noreply.github.com>
2026-09-18 15:54:50 -0400

    Setup user in archinstall with expiring password

---
+++ archinstall/README.md
@@ -0,0 +1,7 @@
+# archinstall instructions
+
+archinstall \
+    --config https://raw.githubusercontent.com/cory-miller/dotfiles/main/archinstall/config.json \
+    --creds https://raw.githubusercontent.com/cory-miller/dotfiles/main/archinstall/creds.json \
+    --script https://raw.githubusercontent.com/cory-miller/dotfiles/main/archinstall/post_install.sh
+
--- archinstall/post_install.sh
+++ archinstall/post_install.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
+TARGET_USER="cory"
 DOTFILES_REPO="https://github.com/cory-miller/dotfiles.git"
-TARGET_DIR="$HOME/dotfiles"
+TARGET_DIR="/home/$TARGET_USER/dotfiles"
 
 echo "==> Starting Post-Install Setup..."
 
-# 1. Enable multilib repository (required for Steam)
+# Enable multilib repository (required for Steam)
 echo "==> Enabling [multilib] repository in pacman.conf..."
 if ! grep -q "^\[multilib\]" /etc/pacman.conf; then
   sudo sed -i '/^#\[multilib\]/{s/^#//;n;s/^#//}' /etc/pacman.conf
   sudo pacman -Sy
 fi
 
-# 2. Enable DRM Kernel Mode Setting for Nvidia Wayland
+# Enable DRM Kernel Mode Setting for Nvidia Wayland
 echo "==> Configuring Nvidia DRM Modesetting for systemd-boot..."
 ENTRY_FILE=$(find /boot/loader/entries/ -name "*.conf" | head -n 1)
 
@@ -26,35 +27,40 @@ else
   echo "    [WARNING] No systemd-boot entry file found. Configure nvidia_drm.modeset=1 manually."
 fi
 
-# 3. Set default shell to ZSH
+# Set default shell to ZSH
 echo "==> Setting default shell to ZSH..."
 if [ "$SHELL" != "$(which zsh)" ]; then
-  chsh -s "$(which zsh)"
+  chsh -s "$(which zsh)" "$TARGET_USER"
 fi
 
-# 4. Clone dotfiles and run Stow
-echo "==> Cloning dotfiles..."
-if [ ! -d "$TARGET_DIR" ]; then
-  git clone "$DOTFILES_REPO" "$TARGET_DIR"
-fi
+# Clone dotfiles and run Stow
+echo "==> Setting up dotfiles..."
+sudo -u "$TARGET_USER" bash -c "
+    if [ ! -d "$TARGET_DIR" ]; then
+      git clone "$DOTFILES_REPO" "$TARGET_DIR"
+    fi
+    cd '$TARGET_DIR'
+    stow -v -t '/home/$TARGET_USER' nvim zsh
+"
 
-echo "==> Stowing configurations..."
-cd "$TARGET_DIR"
-stow -v -t "$HOME" nvim zsh
-
-# 5. Build and Install Paru (AUR Helper)
+# Build and Install Paru (AUR Helper)
 echo "==> Building and installing paru..."
-BUILD_DIR=$(mktemp -d)
-git clone https://aur.archlinux.org/paru-bin.git "$BUILD_DIR/paru"
-cd "$BUILD_DIR/paru"
-makepkg -si --noconfirm
-rm -rf "$BUILD_DIR"
+sudo -u "$TARGET_USER" bash -c "
+  BUILD_DIR=\$(mktemp -d)
+  git clone https://aur.archlinux.org/paru-bin.git \"\$BUILD_DIR/paru\"
+  cd \"\$BUILD_DIR/paru\"
+  makepkg -si --noconfirm
+  rm -rf \"\$BUILD_DIR\"
+"
 
-# 6. Install AUR Packages (Odin and Faugus Launcher)
+# Install AUR Packages
 echo "==> Installing AUR packages (Odin & Faugus Launcher)..."
-paru -S --noconfirm odin-git faugus-launcher
+sudo -u "$TARGET_USER" paru -S --noconfirm odin-git faugus-launcher ghostty vivaldi
 
-# 7. Enable essential system services
+# Expire password to force change on login
+chage -d 0 "$TARGET_USER"
+
+# Enable essential system services
 echo "==> Enabling System Services..."
 sudo systemctl enable NetworkManager.service
 sudo systemctl enable sddm.service
